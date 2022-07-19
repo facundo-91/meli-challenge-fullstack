41795c5f972ac1392bbeab6150492621b4e6aae4
Facundo Benitez <facundob91@gmail.com>
2022-07-19 02:23:16 -0300

    update home page with personal info

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,31 @@
 const Home = () => {
 	return (
-		<div>
-			<h1 className="text-lg text-center">Home Page</h1>
+		<div className="flex h-[calc(100vh-87px)] items-center justify-center bg-[#f5f5f5] lg:h-[calc(100vh-100px)] lg:bg-[#ededed]">
+			<div className="text-center">
+				<h1 className="text-xl font-bold">Challenge Fullstack de MeLi</h1>
+				<h2 className=" text-lg">Contacto:</h2>
+				<a
+					className="mx-2 text-meli-blue"
+					href="https://facundo-91.github.io"
+					rel="noreferrer"
+					target="_blank">
+					Portfolio
+				</a>
+				<a
+					className="mx-2 text-meli-blue"
+					href="https://github.com/facundo-91"
+					rel="noreferrer"
+					target="_blank">
+					Github
+				</a>
+				<a
+					className="mx-2 text-meli-blue"
+					href="https://www.linkedin.com/in/facundo-91/"
+					rel="noreferrer"
+					target="_blank">
+					LinkedIn
+				</a>
+			</div>
 		</div>
 	);
 };

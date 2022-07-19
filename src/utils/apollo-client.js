1eda3372ac414d22c210785385ce25c358fdbc78
Facundo Benitez <facundob91@gmail.com>
2022-07-19 02:31:52 -0300

    update apollo client url to production

--- src/utils/apollo-client.js
+++ src/utils/apollo-client.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-	uri: "http://localhost:3000/api/graphql",
+	uri: "https://meli-challenge-fullstack.vercel.app/api/graphql",
 	cache: new InMemoryCache(),
 });
 

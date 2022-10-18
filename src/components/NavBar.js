a3f685ee69b23021a7940d0e7cf31743b60da66f
Facundo Benitez <facundob91@gmail.com>
2022-10-18 16:30:54 -0300

    fix mp image for dark mode

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,6 @@ import Link from "next/link";
 import Image from "next/image";
 import logoSmall from "../../public/assets/logo__small.png";
 import logoLarge from "../../public/assets/logo__large_plus.png";
-import mercadoPuntos from "../../public/assets/mercado_puntos.webp";
 
 const NavBar = () => {
 	const [searchInput, setSearchInput] = useState("");
@@ -224,8 +223,11 @@ const NavBar = () => {
 							</button>
 						</div>
 					</form>
-					<div className="mr-2.5 hidden max-h-10 max-w-[340px] lg:block dark:lg:hidden">
-						<Image priority alt="ML Logo" height="78" src={mercadoPuntos} width="680" />
+					<div className="mr-2.5 hidden items-center lg:flex">
+						<svg className="mr-3 mb-1 h-[22px] w-[22px] fill-current" viewBox="0 0 16 16">
+							<path d="M3 2.5a2.5 2.5 0 0 1 5 0 2.5 2.5 0 0 1 5 0v.006c0 .07 0 .27-.038.494H15a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1v7.5a1.5 1.5 0 0 1-1.5 1.5h-11A1.5 1.5 0 0 1 1 14.5V7a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h2.038A2.968 2.968 0 0 1 3 2.506V2.5zm1.068.5H7v-.5a1.5 1.5 0 1 0-3 0c0 .085.002.274.045.43a.522.522 0 0 0 .023.07zM9 3h2.932a.56.56 0 0 0 .023-.07c.043-.156.045-.345.045-.43a1.5 1.5 0 0 0-3 0V3zM1 4v2h6V4H1zm8 0v2h6V4H9zm5 3H9v8h4.5a.5.5 0 0 0 .5-.5V7zm-7 8V7H2v7.5a.5.5 0 0 0 .5.5H7z" />
+						</svg>
+						<p className="select-none text-[17px]">Envíos gratis en 24 hs a partir de $4.000</p>
 					</div>
 					<div className="flex h-12 w-[45px] flex-shrink-0 items-center justify-center lg:hidden">
 						<span className="font-navigation text-lg leading-[50px] before:content-['\EA1F'] dark:text-white lg:text-base lg:leading-[27px]" />
